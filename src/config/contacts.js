737ad51477e39b9047265f32434baebbbe39f6c2
Belal Elsabbagh <belsabbagh@gmail.com>
2025-12-30 09:03:26 +0200

    formatting

--- src/config/contacts.js
+++ src/config/contacts.js
@@ -1,16 +1,16 @@
 const github = {
   name: 'GitHub',
-  link: 'https://github.com/belsabbagh'
+  link: 'https://github.com/belsabbagh',
 };
 
 const linkedin = {
   name: 'LinkedIn',
-  link: 'https://www.linkedin.com/in/belsabbagh/'
+  link: 'https://www.linkedin.com/in/belsabbagh/',
 };
 
 const email = {
   name: 'Email',
-  link: 'mailto:belsabbagh@gmail.com'
+  link: 'mailto:belsabbagh@gmail.com',
 };
 
 export default { github, linkedin, email };
--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,24 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		csp: {
-			directives: {
-				'script-src': [
-					'self', 
-				],
-				'object-src': ['none'],
-				'base-uri': ['self'],
-				'style-src': ['self', 'unsafe-inline'],
-				// 'require-trusted-types-for': ['script'],
-			},
-			// must be specified with either the `report-uri` or `report-to` directives, or both
-			reportOnly: {
-				'script-src': ['self'],
-				'report-uri': ['/']
-			}
-		}
-	},
+  kit: {
+    adapter: adapter(),
+    csp: {
+      directives: {
+        'script-src': ['self'],
+        'object-src': ['none'],
+        'base-uri': ['self'],
+        'style-src': ['self', 'unsafe-inline'],
+        // 'require-trusted-types-for': ['script'],
+      },
+      // must be specified with either the `report-uri` or `report-to` directives, or both
+      reportOnly: {
+        'script-src': ['self'],
+        'report-uri': ['/'],
+      },
+    },
+  },
 };
 
 export default config;
