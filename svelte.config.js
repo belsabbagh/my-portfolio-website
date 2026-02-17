cb61136c2d3ccfa2402d2fc0017d5457eae3f941
Belal Elsabbagh <belal@belsabbagh.me>
2026-02-17 22:09:20 +0200

    favicorns

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,25 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-    csp: {
-      mode: 'hash',
-      directives: {
-        'script-src': ['self'],
-        'object-src': ['none'],
-        'base-uri': ['self'],
-        'style-src': ['self', 'unsafe-inline'],
-        // 'require-trusted-types-for': ['script'],
-      },
-      // must be specified with either the `report-uri` or `report-to` directives, or both
-      reportOnly: {
-        'script-src': ['self'],
-        'report-uri': ['/'],
-      },
-    },
-  },
+	kit: {
+		adapter: adapter(),
+		csp: {
+			mode: "nonce",
+			directives: {
+				"script-src": ["self"],
+				"object-src": ["none"],
+				"base-uri": ["self"],
+				"style-src": ["self", "unsafe-inline"],
+				// 'require-trusted-types-for': ['script'],
+			},
+			// must be specified with either the `report-uri` or `report-to` directives, or both
+			reportOnly: {
+				"script-src": ["self"],
+				"report-uri": ["/"],
+			},
+		},
+	},
 };
 
 export default config;
